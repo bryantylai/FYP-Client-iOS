56bec40932f6d421da206c7069780756ee3c217f
Wicky Lim <wickylim@Wickys-MacBook-Pro.local>
2014-03-12 23:02:00 +0800

    Create project

---
+++ FYP/FYP/HFAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  HFAppDelegate.h
+//  FYP
+//
+//  Created by Wicky Lim on 3/12/14.
+//  Copyright (c) 2014 Wicky Lim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HFAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ FYP/FYP/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  FYP
+//
+//  Created by Wicky Lim on 3/12/14.
+//  Copyright (c) 2014 Wicky Lim. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "HFAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([HFAppDelegate class]));
+    }
+}
